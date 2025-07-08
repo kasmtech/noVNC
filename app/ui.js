6e041c71e76bf43c9bfdd00be461859ea96b89e4
El <eldargafiyatov@gmail.com>
2025-07-08 14:37:04 +0500

    VNC-212 Fix monitors addition issue

--- app/ui.js
+++ app/ui.js
@@ -1458,6 +1458,8 @@ const UI = {
         }
         url += '/' + path;
 
+        UI.monitors = [];
+        UI.sortedMonitors = [];
         UI.rfb = new RFB(document.getElementById('noVNC_container'),
                         document.getElementById('noVNC_keyboardinput'),
                         url,
@@ -1657,6 +1659,8 @@ const UI = {
         UI.connected = false;
 
         UI.rfb = undefined;
+        UI.monitors = [];
+        UI.sortedMonitors = [];
 
         if (!e.detail.clean) {
             UI.updateVisualState('disconnected');
@@ -1969,11 +1973,13 @@ const UI = {
 
     openDisplays() {
         document.getElementById('noVNC_displays').classList.add("noVNC_open");
-        if (UI.monitors.length < 1 ) {
+        if (UI.monitors.length < 1  && UI.rfb) {
             let screenPlan = UI.rfb.getScreenPlan();
-            UI.initMonitors(screenPlan)
+            UI.initMonitors(screenPlan);
         }
-        UI.displayMonitors()
+        if (UI.monitors.length > 0) {
+            UI.displayMonitors()
+        }
     },
 
     closeDisplays() {
@@ -2106,6 +2112,9 @@ const UI = {
     recenter() {
         const monitors = UI.sortedMonitors
         UI.removeSpaces()
+        if (!monitors.length) {
+            return;
+        }
         const { startLeft, startTop } = UI.getSizes(monitors)
 
         for (var i = 0; i < monitors.length; i++) {
@@ -2183,11 +2192,12 @@ const UI = {
     },
 
     getSizes(monitors) {
-        const { canvasWidth, canvasHeight } = UI.multiMonitorSettings()
-        let top = monitors[0].y
-        let left = monitors[0].x
-        let width = monitors[0].w
-        let height = monitors[0].h
+        const { canvasWidth, canvasHeight } = UI.multiMonitorSettings();
+
+        let top = monitors[0].y;
+        let left = monitors[0].x;
+        let width = monitors[0].w;
+        let height = monitors[0].h;
         for (var i = 0; i < monitors.length; i++) {
             var m = monitors[i];
             if (m.x < left) {

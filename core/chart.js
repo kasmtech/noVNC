8241c16a811e621f6ca8fee4a62b53577236b315
El <eldargafiyatov@gmail.com>
2025-11-18 03:06:31 +0500

    VNC-270 Add message type constants and chart for basic chart rendering

---
+++ core/chart.js
@@ -0,0 +1,46 @@
+class BasicChart {
+    static CHART_WIDTH = 200;
+    static CHART_HEIGHT = 50;
+    static CHART_MAX_POINTS = 60;
+    static FPS_CHART_MAX_FPS_VALUE = 120;
+
+    constructor(chartId, maxPoints = CHART_MAX_POINTS, maxValue = FPS_CHART_MAX_FPS_VALUE) {
+        this.id = chartId;
+        this.data = [];
+        this.maxPoints = maxPoints;
+        this.maxValue = maxValue;
+        this.width = this.CHART_WIDTH;
+        this.height = this.CHART_HEIGHT;
+    }
+
+    generateChartPoints() {
+        if (this.data.length === 0) return '';
+
+        const stepX = this.width / (this.maxPoints - 1);
+        const scaleY = this.height / this.maxValue;
+
+        let d = `M 0 ${this.height}`;
+
+        for (let i = 0; i < this.data.length; i++) {
+            const x = i * stepX;
+            const y = this.height - this.data[i] * scaleY;
+            d += ` L ${x} ${y}`;
+        }
+
+        d += ` L ${(this.data.length - 1) * stepX} ${this.height} L 0 ${this.height} Z`;
+
+        return d;
+    }
+
+    update(value) {
+        this.data.push(value);
+        if (this.data.length > this.maxPoints) {
+            this.data.shift();
+        }
+
+        const path = document.getElementById(this.id);
+        if (path) {
+            path.setAttribute('d', this.generateChartPoints());
+        }
+    }
+}
---
+++ core/messages.js
@@ -0,0 +1,9 @@
+export const messages = {
+    msgTypeRequestStats: 178,
+    msgTypeFrameStats: 179,
+
+    CLIENT_MSG_TYPE_KEEPALIVE: 184,
+    SERVER_MSG_TYPE_DISCONNECT_NOTIFY: 185,
+
+    msgTypeNetworkStats: 186
+};

804a768c4045d2c5dfc318ace0ab3d9d2d50de2c
El <eldargafiyatov@gmail.com>
2025-12-10 08:53:49 +0000

    VNC-151 Fix video stream quality mapping logic

--- app/ui.js
+++ app/ui.js
@@ -1957,7 +1957,7 @@ const UI = {
                             UI.updateQuality();
                         }
                     } else {
-                        const mappedValue = 60 - (value * 15);
+                        const mappedValue = value * 15;
                         UI.forceSetting(UI_SETTINGS.VIDEO_STREAM_QUALITY, mappedValue, false);
                         UI.updateQuality();
                     }

7f7d974925c5f99fbc409f3b703519a29b37ba7a
Matt McClaskey <matt@example.com>
2026-05-20 09:31:02 +0000

    revert unrelated changes

--- app/ui.js
+++ app/ui.js
@@ -915,13 +915,12 @@ const UI = {
         if (mode !== encodings.pseudoEncodingStreamingModeJpegWebp) {
             const config = configuration || UI.rfb?.videoCodecConfigurations[mode];
 
-            // TODO: This is a temporary workaround, which should be removed once host window starts sending quality preset
             if (WebUtil.isInsideKasmVDI()) {
                 const settingValue = UI.rfb?.videoCodecConfigurations[mode].presets;
                 if (settingValue) {
-                    const quality = WebUtil.readSetting('video_quality', 2);
+                    const quality = parseInt(WebUtil.readSetting('video_quality'));
                     const curQualityValue = parseInt(UI.getSetting(UI_SETTINGS.VIDEO_STREAM_QUALITY));
-                    if (curQualityValue !== settingValue[quality]) {
+                    if (settingValue[quality] !== undefined && curQualityValue !== settingValue[quality]) {
                         UI.forceSetting(UI_SETTINGS.VIDEO_STREAM_QUALITY, settingValue[quality], false);
                     }
                 }

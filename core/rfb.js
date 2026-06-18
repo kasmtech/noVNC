c0fb7702182275f7c95a379737e376b102734629
El <eldargafiyatov@gmail.com>
2026-06-18 12:18:52 +0000

    VNC-393 Add configurable touch mode to support native and gesture handling

--- core/rfb.js
+++ core/rfb.js
@@ -191,6 +191,7 @@ export default class RFB extends EventTargetMixin {
         this._clipboardServerCapabilitiesFormats = {};
 
         this._threading = true;
+        this._touchMode = 'native';
 
         // Internal objects
         this._sock = null;              // Websock object
@@ -782,6 +783,13 @@ export default class RFB extends EventTargetMixin {
         if (value !== this._threading) {
             this._threading = value;
             this._display.threading = value;
+        }
+    }
+
+    get touchMode() { return this._touchMode; }
+    set touchMode(value) {
+        if (value !== this._touchMode) {
+            this._touchMode = value;
         }
     }
 
@@ -2693,6 +2701,10 @@ export default class RFB extends EventTargetMixin {
     }
 
     _handleNativeTouch(ev) {
+        if (this._touchMode !== 'native') {
+            return;
+        }
+
         if (!this.isConnected || this._viewOnly || !this._isPrimaryDisplay)
             return;
 
@@ -2781,7 +2793,9 @@ export default class RFB extends EventTargetMixin {
     }
 
     _handleGesture(ev) {
-        return; // native touch active — gestures disabled
+        if (this._touchMode === 'native') {
+            return;
+        }
 
         let magnitude;
 

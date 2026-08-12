37d6c9cd4986c0dd37214628ac08499625f2f975
El <eldargafiyatov@gmail.com>
2026-08-12 10:35:35 +0000

    VNC-393 Update message types after rebase

--- core/messages.js
+++ core/messages.js
@@ -1,17 +1,17 @@
 export const messages = {
     msgTypeRequestStats: 178,
     msgTypeFrameStats: 179,
-    msgTypeNetworkStats: 190,
-    msgTypeSystemStats: 191,
     msgTypeVideoEncoders: 184,
     msgTypeKeepAlive: 185,
     msgTypeServerDisconnect: 186,
     msgTypeForceGameMode: 187,
     msgTypeDirectMouseEvent: 188,
     msgTypeLatencyMeasurement: 189,
-    msgTypeTouchSupported: 189,
-    msgTypeTouchEvent: 190,
-    msgTypeTextInputFocus: 191,
+    msgTypeNetworkStats: 190,
+    msgTypeSystemStats: 191,
+    msgTypeTouchSupported: 192,
+    msgTypeTouchEvent: 193,
+    msgTypeTextInputFocus: 194,
     msgTypeUserAddedToSession: 253,
     msgTypeUserRemovedFromSession: 254
 };

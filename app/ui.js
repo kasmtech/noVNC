6e0621b7d1f091865ebb49321473ee4d6c7e07a9
El <eldargafiyatov@gmail.com>
2026-09-14 12:13:13 +0000

    VNC-393 Update codec preferences and simplify streaming mode matching logic

--- app/ui.js
+++ app/ui.js
@@ -1106,7 +1106,7 @@ const UI = {
 
         if (preferredMatch.length > 0) {
             if (result === encodings.pseudoEncodingStreamingModeJpegWebp) {
-                result = Math.min(...preferredMatch);
+                result = preferredMatch[0];
             }
         }
 

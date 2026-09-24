2ebf2e500833c603e8f894e9330df3669d711e4e
El <eldargafiyatov@gmail.com>
2026-09-24 13:29:56 +0000

    VNC-130 Remove redundant IME enablement checks in UI initialization functions

--- app/ui.js
+++ app/ui.js
@@ -2158,10 +2158,6 @@ const UI = {
         UI.showStats();
         UI.updateVisualState('connected');
 
-        if (UI.getSetting('enable_ime')) {
-            UI.rfb.keyboard.enableIME = true;
-        }
-
         // Do this last because it can only be used on rendered elements
         UI.rfb.focus();
     },
